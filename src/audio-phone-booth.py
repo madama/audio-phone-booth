e6b4b8f56cd68c4deccb3d02871ee96541d16455
Daniele Madama <danielemadama@danynb.home>
2019-03-21 22:13:43 +0100

    intercept keys

--- src/audio-phone-booth.py
+++ src/audio-phone-booth.py
@@ -1,13 +1,12 @@
 #import RPi.GPIO as GPIO
-#import time
-import signal
-import sys
 from pad4pi import rpi_gpio
+from pygame import mixer
+import time
 
 #GPIO.setmode(GPIO.BCM)
 KEYPAD = [
     ["F1", "", "F2", "F3", ""],
-    ["Down", 7, "Circles", 8, 9]
+    ["Down", "7", "Circles", "8", "9"]
 ]
 
 ROW_PINS = [4, 14] #15, 17, 18
@@ -16,13 +15,7 @@ COL_PINS = [22, 23, 24, 25, 8]
 factory = rpi_gpio.KeypadFactory()
 
 keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PINS)
-
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    keypad.cleanup()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
+mixer.init()
 
 print("Init done!")
 
@@ -30,3 +23,11 @@ def printKey(key):
     print(key)
 
 keypad.registerKeyPressHandler(printKey)
+
+try:
+    while(True):
+        time.sleep(0.2)
+except:
+    keypad.cleanup()
+    print("Quit!")
+
