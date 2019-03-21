6f98fb06e5cfa5920f5f78a152849b6bf5f86291
Daniele Madama <danielemadama@danynb.home>
2019-03-21 23:13:44 +0100

    Improvements

--- src/audio-phone-booth.py
+++ src/audio-phone-booth.py
@@ -1,15 +1,16 @@
-#import RPi.GPIO as GPIO
 from pad4pi import rpi_gpio
 from pygame import mixer
 import time
 
-#GPIO.setmode(GPIO.BCM)
 KEYPAD = [
     ["F1", "", "F2", "F3", ""],
-    ["Down", "7", "Circles", "8", "9"]
+    ["Down", "7", "Circles", "8", "9"],
+    ["Red", "*", "Volume", "0", "#"],
+    ["Up", "1", "L", "2", "3"],
+    ["Left", "4", "Right", "5", "6"]
 ]
 
-ROW_PINS = [4, 14] #15, 17, 18
+ROW_PINS = [4, 14, 15, 17, 18]
 COL_PINS = [22, 23, 24, 25, 8]
 
 factory = rpi_gpio.KeypadFactory()
@@ -18,9 +19,21 @@ keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PI
 mixer.init()
 
 print("Init done!")
+print("Waiting for keys...")
+
+track = ""
 
 def printKey(key):
     print(key)
+    if (key=="#")
+        print("Playing {}.mp3".format(track))
+    elif (key=="*")
+        print("Reset")
+        track = ""
+    else:
+        track += key
+        print("track: ", track)
+
 
 keypad.registerKeyPressHandler(printKey)
 
